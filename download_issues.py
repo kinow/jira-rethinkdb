afc328f1edc339c824a819de3615931c6108621f
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-07 21:43:48 +1200

    Update search and add rethinkdb call

--- download_issues.py
+++ download_issues.py
@@ -12,6 +12,7 @@ with open('.env', 'r') as f:
 
 from jira import JIRA
 import rethinkdb as r
+from pprint import pprint
 
 def filter_issues(issues):
     filtered = []
@@ -29,6 +30,12 @@ def filter_issues(issues):
         new_issue['assignee_key'] = issue.get('fields', {}).get('assignee', {}).get('key', 'N/A')
         new_issue['assignee_name'] = issue.get('fields', {}).get('assignee', {}).get('displayName', 'N/A')
         new_issue['updated'] = issue.get('fields', {}).get('updated', '')
+        new_issue['status'] = issue.get('fields', {}).get('status', {}).get('name', 'N/A')
+        new_issue['description'] = issue.get('fields', {}).get('description', 'N/A')
+        new_issue['summary'] = issue.get('fields', {}).get('summary', 'N/A')
+        new_issue['creator_key'] = issue.get('fields', {}).get('creator', {}).get('key', 'N/A')
+        new_issue['creator_name'] = issue.get('fields', {}).get('creator', {}).get('displayName', 'N/A')
+        new_issue['subtasks'] = issue.get('fields', {}).get('subtasks', [])
 
         filtered.append(new_issue)
     return filtered
@@ -39,15 +46,38 @@ def main():
     }
     jira = JIRA(options, basic_auth=(config['USERNAME'], config['PASSWORD']))
 
-    issues_in_month = jira.search_issues("created >= '2014-03-01' AND created < '2014-04-01'", json_result=True)
+    total_issues = 0
+
+    months = [
+        '2015-03',
+        '2015-04',
+        '2015-05',
+        '2015-06',
+        '2015-07',
+        '2015-08',
+        '2015-09',
+        '2015-10',
+        '2015-11',
+        '2015-12',
+        '2016-01',
+        '2016-02',
+        '2016-03',
+    ]
+
+    issues_in_month = jira.search_issues("created >= '2014-03-01' AND created < '2014-04-01'", maxResults=1000, json_result=True)
     issues = issues_in_month['issues']
+    total_issues = len(issues)
 
     filtered_issues = filter_issues(issues)
 
-    print(filtered_issues)
+    pprint(filtered_issues)
 
-    # r.connect(RETHINKDB, 28015, db='issues').repl()
-    # r.table('issues').insert(issues).run()
+    print("Successfully downloaded %d issues" % total_issues)
+    print("Loading issues into RethinkDB")
+
+    r.connect(config['RETHINKDB'], 28015, db='issues').repl()
+    r.table('issues').delete()
+    r.table('issues').insert(issues).run()
 
 if __name__ == '__main__':
     main()
