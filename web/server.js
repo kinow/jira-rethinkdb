91b78bfbd1a21a0d9a6aedabfd6612c51d58739f
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-08 02:06:49 +1200

    Report #2

--- web/server.js
+++ web/server.js
@@ -18,7 +18,7 @@ r.connect( {host: '172.17.0.2', port: 28015}, function(err, conn) {
     connection = conn;
 });
 
-app.get('/', function (req, res) {
+app.get('/report_1', function (req, res) {
     var perMonth = r.db('jira')
         .table('issues')
         .group(
@@ -37,6 +37,32 @@ app.get('/', function (req, res) {
                     data.push({ "Count": entry.reduction, "Type": entry.group[1], "Month": entry.group[0] });
                 }
                 res.render('report_1', {'data': JSON.stringify(data) });
+            });
+        })
+    ;
+});
+
+app.get('/report_2', function (req, res) {
+    var perMonth = r.db('jira')
+        .table('issues')
+        .group(
+            r.row('created').slice(0, 10).do(function(s) {
+              return r.ISO8601(s, {defaultTimezone: '+00:00'}).dayOfWeek();
+            }),
+            r.row('type')
+        )
+        .count()
+        .run(connection, function(err, cursor) {
+            if (err) throw err;
+            cursor.toArray(function(err, result) {
+                if (err) throw err;
+                //console.log(JSON.stringify(result, null, 2));
+                var data = [];
+                for (var i = 0; i < result.length; i++) {
+                    var entry = result[i];
+                    data.push({ "Count": entry.reduction, "Type": entry.group[1], "DayOfWeek": entry.group[0] });
+                }
+                res.render('report_2', {'data': JSON.stringify(data) });
             });
         })
     ;
